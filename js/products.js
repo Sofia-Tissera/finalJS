709fd0c0fe0c26217ca953010ce65370fe013715
SofiaTissera <sofiatissera@yahoo.com.ar>
2023-06-22 23:22:16 -0300

    first commit

---
+++ js/products.js
@@ -0,0 +1,31 @@
+const productos = [{
+        id: 1,
+        nombre: "Harina",
+        precio: 50,
+        img: "./img/harina.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Aceite",
+        precio: 100,
+        img: "./img/aceite.jpg",
+        cantidad: 1,
+
+    },
+    {
+        id: 3,
+        nombre: "Galletitas",
+        precio: 30,
+        img: "./img/galletitas.jpg",
+        cantidad: 1,
+
+    },
+    {
+        id: 4,
+        nombre: "Leche",
+        precio: 150,
+        img: "./img/leche.jpg",
+        cantidad: 1,
+    },
+];
